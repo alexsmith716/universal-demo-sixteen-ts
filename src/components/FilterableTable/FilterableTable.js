8d95a990a7d07dfde8d934e2de53a8865e6637c7
alexsmith716 <alexsmith716@outlook.com>
2020-02-01 17:04:54 -0500

    refactor: Modified mockAPI info reducer using then method promises not async await generator pattern.

--- src/components/FilterableTable/FilterableTable.js
+++ src/components/FilterableTable/FilterableTable.js
@@ -26,158 +26,10 @@ import { promiseGenerator } from '../../utils/promiseGenerator';
   (dispatch, { multireducerKey: key  }) => bindActionCreators({ ...filterableTableActions }, dispatch, key)
 )
 
-// closure, global scope, function scope, block scope, arrow functions
-// var, let, const and variable declarations in loops
-
-// scope, Function Invocation, Method Invocation, Constructor Invocation
-
-// been in an ES6 env so long i need REVIEW again!
-
-// defined component (ES6 class)
-// https://reactjs.org/docs/react-without-es6.html#autobinding
-
-// Autobinding:
-
-// =======================================================================================================
-// In React components declared as ES6 classes, methods follow the same semantics as regular ES6 classes. 
-// >>>>>>>>>>>>>>>> This means that they don’t automatically bind this to the instance <<<<<<<<<<<<<<<<<<<
-// >>>>>>>>>>>>>>>>   You’ll have to explicitly use .bind(this) in the constructor     <<<<<<<<<<<<<<<<<<<
-// =======================================================================================================
-
-// app ES6 environment is using:
-//  "@babel/plugin-proposal-class-properties"
-// which binds all methods
-// So, no need to use .bind(this) in the constructor
-
-// https://reactjs.org/docs/forms.html
-// https://reactjs.org/docs/uncontrolled-components.html
-// react controlled component onChange
-// 'onChange' handler captures 'onChange' event for '<div>' and 'children'
-
 class FilterableTable extends Component {
-
-  // Autobinding NOT NEEDED > ES6 env > "@babel/plugin-proposal-class-properties"
-  // constructor(props){
-  //   super(props);
-  //   // bind event handler method to an instance
-  //   this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-  //   this.handleInStockChange = this.handleInStockChange.bind(this);
-  //   this.handleDropdownChange = this.handleDropdownChange.bind(this);
-  // }
-
-  // static propTypes = {
-  //   // dropDownOptionSelected: PropTypes.string,
-  //   // error: PropTypes.bool,
-  //   // errorResponse: PropTypes.object,
-  //   // loading: PropTypes.bool,
-  //   // // data: PropTypes. .isRequired,
-  //   // // optionsArray: PropTypes.array.isRequired,
-  //   // // description: PropTypes.string,
-  //   // filterText: PropTypes.string,
-  //   // inStockOnly: PropTypes.bool,
-  //   // selectedOption: PropTypes.func.isRequired,
-  //   // load: PropTypes.func.isRequired,
-  // };
-
-  // static defaultProps = {};
-
-  // state = {
-  //   prevProps: this.props
-  // };
-
-  // ==============================================================================================
-
-
-  // MOUNTING (instance of a component is being created and inserted into the DOM)
-  // ----------------------------------------------------------------------------------------------
-  // ----------------------------------------------------------------------------------------------
-
-  // constructor()
-
-  // static getDerivedStateFromProps(props, state)
-
-  // render()
-
-  // RE: component that subscribes to an external event dispatcher
-  // Event listeners are only safe to add after mount,
-  // So they won't leak if mount is interrupted or errors.
-  // External values could change between render and mount,
-  // In some cases it may be important to handle this case.
-  // componentDidMount() {
-  //   const { data } = this.props;
-  //   console.log('>>>>>>>>>>>>>>>> FilterableTable > componentDidMount() > data1: ', data);
-  //   if (data === null) {
-  //     console.log('>>>>>>>>>>>>>>>> FilterableTable > componentDidMount() > data2');
-  //   } else {
-  //     console.log('>>>>>>>>>>>>>>>> FilterableTable > componentDidMount() > data3');
-  //   }
-  // }
-
-  // ==============================================================================================
-
-
-  // UPDATING (update was caused by changes to props or state. component is being re-rendered.)
-  // ----------------------------------------------------------------------------------------------
-  // ----------------------------------------------------------------------------------------------
-
-  // derive state:
-  // enables a component to update its internal state as the result of changes in props
-  // invoked right before calling the render method, both on the initial mount and on subsequent updates
-  // invoked after a component is instantiated as well as before it is re-rendered
-  // --------------------------------------------------------------------------------
-  // static getDerivedStateFromProps(props, state) {
-  //   const { prevProps } = state;
-  //   // chance to compare incoming props to previous props
-  //   console.log('>>>>>>>>>>>>>>>> FilterableTable > getDerivedStateFromProps() <<<<<<<<<<<<<<<<<<<<<<');
-  //   console.log('>>>>>>>>>>>>>>>> FilterableTable > getDerivedStateFromProps() > prevProps: ', prevProps);
-  //   // return {
-  //   //   prevProps: props
-  //   // };
-  //   return null;
-  // };
-
-  // invoked before rendering when new props or state are being received (default: true)
-  // method not called for initial render
-  // let react know if a component's output is not affected by the current change in state or props
-  // evaluate "true" ? re-render
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   console.log('>>>>>>>>>>>>>>>> FilterableTable > shouldComponentUpdate()?? > nextProps: ', nextProps);
-  //   console.log('>>>>>>>>>>>>>>>> FilterableTable > shouldComponentUpdate()?? > nextState: ', nextState);
-  //   return nextProps;
-  // };
-
-  // 'render()' will not be invoked if 'shouldComponentUpdate()' returns false
-  // render()
-
-  // there may be delays between “render” phase lifecycles (like render) 
-  //  and “commit” phase lifecycles (like getSnapshotBeforeUpdate and componentDidUpdate)
-
-  // invoked before most recently rendered output is committed to the DOM
-  // enables capturing information from the DOM (e.g. scroll position) before it is changed
-  // getSnapshotBeforeUpdate(prevProps, prevState) {
-  //   return null;
-  // }
-
-  // invoked immediately after updating
-  // not called for the initial render
-  // component has been updated, so do something
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     const { loading, load, dropDownOptionSelected, loaded } = this.props;
-
-    // if (data === prevProps.data) {
-    //   //
-    // }
-
-    // if (data !== prevProps.data) {
-    //   //
-    // }
-
-    // LOAD_FAIL
-    // if (error) {
-    //   console.log('>>>>>>>>>>>>>>>> FilterableTable > componentDidUpdate() > LOAD_FAIL > error: ', error);
-    //   console.log('>>>>>>>>>>>>>>>> FilterableTable > componentDidUpdate() > LOAD_FAIL > errorResponse: ', errorResponse);
-    // }
 
     if (loading) {
       load({ request: dropDownOptionSelected });
@@ -207,38 +59,6 @@ class FilterableTable extends Component {
 
   // ==============================================================================================
 
-
-  // UNMOUNTING (component is being removed from the DOM)
-  // ----------------------------------------------------------------------------------------------
-  // ----------------------------------------------------------------------------------------------
-
-  // componentWillUnmount() {
-  //   //
-  // }
-
-  // ==============================================================================================
-
-
-  // ERROR HANDLING (error during render, in a lifecycle, in the constructor of any child component)
-  // ----------------------------------------------------------------------------------------------
-  // ----------------------------------------------------------------------------------------------
-
-  // invoked after an error has been thrown by a descendant component
-  // receives the error thrown as param and returns a value to update state
-  // static getDerivedStateFromError(error) {
-  //   // Update state so the next render will show the fallback UI.
-  //   // return { hasError: true };
-  //   return;
-  // }
-
-  // invoked after an error has been thrown by a descendant component
-  // used for things like logging errors
-  // componentDidCatch(error, info) {
-  //   console.log('>>>>>>>>>>>>>>>> FilterableTable > componentDidCatch() > info.componentStack: ', info.componentStack);
-  // }
-
-  // ==============================================================================================
-
   handleDropdownChange = (e) => {
     const { actionHandleDropdownChange } = this.props;
     actionHandleDropdownChange({
@@ -260,7 +80,6 @@ class FilterableTable extends Component {
     });
   };
 
-  // 'render()' will not be invoked if 'shouldComponentUpdate()' returns false
   render() {
 
     const styles = require('./scss/FilterableTable.scss');
--- src/redux/modules/info.js
+++ src/redux/modules/info.js
@@ -4,7 +4,7 @@ const LOAD = 'redux-example/info/LOAD';
 const LOAD_SUCCESS = 'redux-example/info/LOAD_SUCCESS';
 const LOAD_FAIL = 'redux-example/info/LOAD_FAIL';
 
-import { mockAPI, getSomeAsyncData } from '../../utils/mockAPI';
+import { postRequestConcatExportASYNC } from '../../utils/mockAPI';
 
 const initialState = {
   loaded: false,
@@ -64,15 +64,44 @@ export function load() {
   // let location = 'https://www.metaweather.com/api/location/2459115/';
   return {
     types: [LOAD, LOAD_SUCCESS, LOAD_FAIL],
-    // await getSomeAsyncData(location);
-    promise: async () => {
-      try {
-        const response = await getSomeAsyncData(location);
-        return response;
-      } catch (error) {
-        return Promise.reject(error);
-        throw error;
-      }
-    }
+    promise: () => postRequestConcatExportASYNC('resolve', true, 6600)
+      .then(
+        result => {
+          console.log('>>>>>>>>>>>>>>>> REDUX > INFO > load() > THEN 1111111 +++++++++++++++++++++++++++');
+          // result.message += ' P6.'
+          return result;
+        }, 
+        error => {
+          // handle rejected promise in action creator (closest rejection handler)
+          console.log('>>>>>>>>>>>>>>>> REDUX > INFO > load() > THEN 2222222 +++++++++++++++++++++++++++');
+          return Promise.reject(error);
+          throw error;
+        }
+      )
+      // .catch(error => {
+      //   // handle rejected promise in action creator (closest rejection handler)
+      //   console.log('>>>>>>>>>>>>>>>> REDUX > INFO > addNewDataFunc() > CATCH:ERROR:', error);
+      //   return Promise.reject(error);
+      //   throw error;
+      // })
   };
 };
+
+// export function load() {
+//   console.log('>>>>>>>>>>>>>>>> REDUX > INFO > load() +++++++++++++++++++++++++++');
+//   let location = 'https://api.github.com/feeds';
+//   // let location = 'https://www.metaweather.com/api/location/2459115/';
+//   return {
+//     types: [LOAD, LOAD_SUCCESS, LOAD_FAIL],
+//     // await getSomeAsyncData(location);
+//     promise: async () => {
+//       try {
+//         const response = await getSomeAsyncData(location);
+//         return response;
+//       } catch (error) {
+//         return Promise.reject(error);
+//         throw error;
+//       }
+//     }
+//   };
+// };
